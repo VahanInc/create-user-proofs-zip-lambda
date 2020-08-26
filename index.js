0663537329f8b68984f3b9e4f0070d964bb191ad
Abhinav Bansal <abhinav@vahan.co>
2020-08-26 13:12:18 +0530

    Added .jpg extension in case extension is not present

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const S3_ZIP_FOLDER = "user-proofs-zips";
 
 //This returns us a stream.. consider it as a real pipe sending fluid to S3 bucket.. Don't forget it
 const streamTo = (bucket, key, streamPassThrough) => {
-	
-	return s3.upload( { Bucket: bucket, 
-		Key: key, 
-		ACL: 'public-read', 
+
+	return s3.upload( { Bucket: bucket,
+		Key: key,
+		ACL: 'public-read',
 		ContentType: 'application/zip',
-		Body: streamPassThrough }, (err, data) => { 
+		Body: streamPassThrough }, (err, data) => {
 		if(err) {
 			console.log("Error streaming data to S3. Error=" + err);
 		}
@@ -32,6 +32,14 @@ const generateRandomString = function(length) {
     }
     return randomString;
 };
+
+const createFileName = function(name) {
+	if(name.indexOf(".") === -1) {
+		return name + '.jpg';
+	} else {
+		return name;
+	}
+}
 
 exports.handler = async (event, context, callback) => {
 
@@ -63,11 +71,11 @@ exports.handler = async (event, context, callback) => {
 
 		    const list = await Promise.all(imageKeys.map(key => new Promise((resolve, reject) => {
 		            s3GetObjectAsync({Bucket:event.s3Bucket, Key:key})
-		                .then(data => resolve( { data: data.Body, name: `${key.split('/').pop()}` } ));
+		                .then(data => resolve( { data: data.Body, name: createFileName(`${key.split('/').pop()}`) } ));
 		        }
-		    ))).catch(err => { 
+		    ))).catch(err => {
 		    	console.log(err);
-		    	throw new Error(err) 
+		    	throw new Error(err)
 		    });
 
 		    const zipKey = `${S3_ZIP_FOLDER}/${userId}_${generateRandomString(6)}.zip`;
@@ -83,15 +91,15 @@ exports.handler = async (event, context, callback) => {
 			        console.log(progress);
 			    });
 
-		    await new Promise((resolve, reject) => { 
+		    await new Promise((resolve, reject) => {
 		        s3Upload.on('close', resolve());
 		        s3Upload.on('end', resolve());
 		        s3Upload.on('error', reject());
-		        
+
 		        archive.pipe(streamPassThrough);
 		        list.forEach(itm => archive.append(itm.data, { name: itm.name }));
-		        archive.finalize();	
-		    }).catch(err => { 
+		        archive.finalize();
+		    }).catch(err => {
 		    	console.log(err);
 		    	throw new Error(err);
 		    });
